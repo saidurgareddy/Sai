a23fa791403aaf78f54f735d75c795bc5d37d5f5
saidurgareddy <32542092+saidurgareddy@users.noreply.github.com>
2017-12-29 01:23:55 -0800

    Create add_two.c
    c program to add two files

---
+++ add_two.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void)
+{
+int  a, b, c;
+printf("enter two numbers");
+scanf("%d%d",&a,&b);
+c=a+b;
+printf("the third number is %d ",c);
+return 0;
+}
